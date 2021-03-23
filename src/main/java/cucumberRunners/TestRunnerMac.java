0f2be6297bf1bc5de1c15e1f4a3c8162af86ecc3
Stefan <40228918+uskka@users.noreply.github.com>
2021-03-23 13:29:41 +0100

    mac runner

---
+++ src/main/java/cucumberRunners/TestRunnerMac.java
@@ -0,0 +1,11 @@
+package cucumberRunners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/main/features",glue = "steps", tags = "@automated")
+
+public class TestRunnerMac {
+}
